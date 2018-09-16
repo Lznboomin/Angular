c2992cdddf379405483035291a303c2794b89302
Endrik L <endrik.laanemagi@khk.ee>
2018-09-16 16:56:51 +0200

    Added routing

---
+++ js/router.js
@@ -0,0 +1,22 @@
+var app = angular.module('myApp', ['ngRoute']);
+app.config(function($routeProvider) {
+    $routeProvider
+    .when('/', {
+        templateUrl: "index.html"
+    })
+    .when('/todo', {
+        templateUrl: "todo.html"
+    })
+    .when('/examples', {
+        templateUrl: "examples.html"
+    })
+    .when('/resources', {
+        templateUrl: "resources.html"
+    })
+    .when('/game', {
+        templateUrl: "game.html"
+    })
+    .when('/feedback', {
+        templateUrl: "feedback.html"
+    });
+});
